2a6c7ff1b4820a248c645a8dc8c5c1a67962869c
MJ <mj.saidy@hva.nl>
2022-10-04 21:06:23 +0200

    backend

---
+++ backend/src/test/java/com/hva/helios/HeliosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hva.helios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeliosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
