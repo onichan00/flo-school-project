13c37086fefc15c54845bbffd0592cac672748ff
Dennis Moes <dennis.moes2@hva.nl>
2022-10-10 12:34:36 +0200

    Added the layout of the profile

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,61 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld class="bg-red-500" msg="Welcome to Your Vue.js App"/>
-
-  <ul class="divide-y divide-gray-200">
-    <li class="py-4 flex">
-      <img class="h-10 w-10 rounded-full" src="" alt="" />
-      <div class="ml-3">
-        <p class="text-sm font-medium text-gray-900">person.name1</p>
-        <p class="text-sm text-gray-500">person.email </p>
-      </div>
-    </li>
-    <li class="py-4 flex">
-      <img  src="" alt="" class="h-10 w-10 rounded-full"/>
-      <div class="ml-3">
-        <p class="text-sm font-medium text-gray-900">person.name2</p>
-        <p class="text-sm text-gray-500">person.email </p>
-      </div>
-    </li>
-    <li class="py-4 flex">
-      <img class="h-10 w-10 rounded-full" src="" alt="" />
-      <div class="ml-3">
-        <p class="text-sm font-medium text-gray-900">person.name1</p>
-        <p class="text-sm text-gray-500">person.email </p>
-      </div>
-    </li>
-    <li class="py-4 flex">
-      <img  src="" alt="" class="h-10 w-10 rounded-full"/>
-      <div class="ml-3">
-        <p class="text-sm font-medium text-gray-900">person.name2</p>
-        <p class="text-sm text-gray-500">person.email </p>
-      </div>
-    </li>
-    <li class="py-4 flex">
-      <img class="h-10 w-10 rounded-full" src="" alt="" />
-      <div class="ml-3">
-        <p class="text-sm font-medium text-gray-900">person.name1</p>
-        <p class="text-sm text-gray-500">person.email </p>
-      </div>
-    </li>
-    <li class="py-4 flex">
-      <img  src="" alt="" class="h-10 w-10 rounded-full"/>
-      <div class="ml-3">
-        <p class="text-sm font-medium text-gray-900">person.name2</p>
-        <p class="text-sm text-gray-500">person.email </p>
-      </div>
-    </li>
-    <router-view></router-view>
-  </ul>
+  <div class="flex flex-col md:flex-row h-screen">
+    <div class="w-full md:w-1/4 bg-purple-300">
+      <p>Hello</p>
+    </div>
+    <div class="w-full md:w-3/4 overflow-y-scroll">
+      <PageHeader title="Specialist Profile"/>
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PageHeader from './components/PageHeader.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PageHeader
   }
 }
 </script>
@@ -67,6 +28,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
