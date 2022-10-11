84839cf7814a395447bdda249369070a3a21ddc4
Ilias aachboun <ili.aachboun1@gmail.com>
2022-10-11 21:16:12 +0200

    Remove second router and use only one

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -4,8 +4,17 @@ import UnknownRoute from "@/components/404-page"
 
 export const router = createRouter({
     history: createWebHashHistory(),
-    routes:[
+    routes: [
         {path: '/', component: HelloWorld},
-        {path: '/:pathMatch(.*)', component: UnknownRoute}
+        {path: '/:pathMatch(.*)', component: UnknownRoute},
+        {
+            path: '/dashboard',
+            name: 'Dashboard',
+            component: () => import('../views/Dashboard.vue')},
+        {
+            path: '/specialists',
+            name: 'Specialists',
+            component: () => import('../views/Specialists.vue')
+        },
     ]
 })
