4c0770d62f4d4a67bac646ce355cc7d3af9bdb75
Dennis Moes <dennis.moes2@hva.nl>
2022-10-22 11:27:10 +0200

    Added 6 florijnOrange colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        florijnOrange: "#ec5a29",
+       florijnOrange: {
+          DEFAULT: "#ec5a29",
+              100: "#ec5827",
+              200: "#cd3d0d",
+              300: "#af1f00",
+              400: "#920000",
+              500: "#760000",
+              600: "#5c0000"
+        },
         florijnGray: "#919194"
       }
     },
