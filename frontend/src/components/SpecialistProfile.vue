40863dcea75460d04472a2befd200585608b2726
Dennis Moes <dennis.moes2@hva.nl>
2022-10-22 11:01:20 +0200

    Small changes

--- frontend/src/components/SpecialistProfile.vue
+++ frontend/src/components/SpecialistProfile.vue
@@ -137,9 +137,7 @@ export default {
   name: 'SpecialistProfile',
   data() {
     return {
-      isEditing: true,
-      disabledInputClass: "bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed",
-      activeInputClass: "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
+      isEditing: false,
       user: {
         firstName: "John",
         secondName: "Michiel",
