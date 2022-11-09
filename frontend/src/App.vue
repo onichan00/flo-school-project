151e25f15a2fd2a85ae820bb94dfc4e1667ae2bd
Simon Vriesema <simon.vriesema@icloud.com>
2022-11-09 17:10:41 +0100

    made frontend of the landing page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <!--  <img alt="Vue logo" src="./assets/logo.png">-->
-  <Sidebar/>
-  <div :style="{ 'margin-left': sidebarWidth }">
-    <PageHeader/>
-    <router-view/>
-    <Footer></Footer>
-  </div>
+<!--  <Sidebar/>-->
+<!--  <div :style="{ 'margin-left': sidebarWidth }">-->
+<!--    <PageHeader/>-->
+<!--    <router-view/>-->
+<!--    <Footer></Footer>-->
+<!--  </div>-->
+  <LandingPage></LandingPage>
   <!--  <HelloWorld class="bg-red-500" msg="Welcome to Your Vue.js App"/>-->
 
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-import Sidebar from "@/components/sidebar/Sidebar.vue";
-import PageHeader from "@/components/PageHeader";
+// import Sidebar from "@/components/sidebar/Sidebar.vue";
+// import PageHeader from "@/components/PageHeader";
 import {sidebarWidth} from '@/components/sidebar/state.js'
-import Footer from "@/components/Footer.vue";
+// import Footer from "@/components/Footer.vue";
+import LandingPage from '@/views/LandingPage.vue'
 
 export default {
   name: 'App',
   components: {
-    Sidebar,
-    Footer,
-    PageHeader
+    // Sidebar,
+    // Footer,
+    // PageHeader,
+    LandingPage
   },
 
   setup() {
