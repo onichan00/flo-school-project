392b26e8149ae3b7cc229b11104506386c810bc2
MJ <mj.saidy@hva.nl>
2022-11-14 16:02:50 +0100

    added cors access to frontend and implemented backend into frontend, but the data still needs to be fully implemented

--- backend/src/main/java/com/hva/helios/rest/UserController.java
+++ backend/src/main/java/com/hva/helios/rest/UserController.java
@@ -3,13 +3,11 @@ package com.hva.helios.rest;
 import com.hva.helios.models.User;
 import com.hva.helios.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4040", maxAge = 3600)
 @RestController
 @RequestMapping("users")
 public class UserController {
