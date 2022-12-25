8c7f777b4155c8cffc560f7a9e534b8c74628172
Simon Vriesema <simon.vriesema@icloud.com>
2022-12-25 14:06:27 +0100

    almost finished settings page

--- frontend/src/views/ClientSettings.vue
+++ frontend/src/views/ClientSettings.vue
@@ -72,7 +72,7 @@
 
       <div class="bg-white w-3/4 rounded-lg m-1 text-left max-h-fit shadow-lg">
 
-        <div class="px-4 py-3 h-auto" v-if="selectedSettings" v-bind="selectedSettings">
+        <div class="px-4 py-3 h-auto" v-if="selectedSettings.id===1" v-bind="selectedSettings">
           <div class="bg-white rounded-lg dark:bg-gray-700">
             <div class="flex flex-col text-left">
               <div class="flex items-start pt-2 mb-1 justify-between rounded-t dark:border-gray-600">
@@ -85,7 +85,7 @@
             <hr>
 
             <form class="h-fit pt-3">
-              <div class="mb-2">
+              <div class="mb-3">
                 <label class="block mb-2 text-sm font-medium text-gray-900" for="file_input">Huidige Profielfoto</label>
                 <img class="mb-2 w-36 h-36 rounded-lg border-2"
                      :src="require('@/assets/img/undraw_coffee_with_friends_3cbj.svg')" alt="Profielfoto">
@@ -95,11 +95,79 @@
                     aria-describedby="file_input_help" id="file_input" type="file">
                 <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">SVG, PNG of JPG</p>
               </div>
-              <div class="grid md:grid-cols-3 md:gap-6">
+              <div class="mb-4">
+                <label for="website"
+                       class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Website URL</label>
+                <p class="block text-left mb-2 text-sm text-gray-600">Schrijf uw website zodat specialisten weten waarvoor zij werken.</p>
+                <input @change="changeButtonToOne" id="website" rows="4" type="url"
+                          class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5"
+                          placeholder="Plak hier de website URL"
+                >
+              </div>
+              <div class="mb-4">
+                <label for="description"
+                       class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Bio</label>
+                <p class="block text-left mb-2 text-sm text-gray-600">Vertel wat over u zelf.</p>
+                <textarea @change="changeButtonToOne" v-model="this.user.bio" id="description" rows="4"
+                          class="block p-2.5 w-full h-24 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-orange-500 focus:border-orange-500"
+                          placeholder="Schrijf hier de beschrijving..."
+                ></textarea>
+              </div>
+            </form>
+
+            <hr>
+
+            <div class="w-full mt-4">
+
+              <div v-if="buttonType === 1" class="float-right space-x-2 border-gray-200 mb-4">
+                <button @click="updateClientProfile"
+                        type="button"
+                        class="p-2 text-md font-medium text-white bg-gradient-to-r from-orange-400 via-orange-500 to-orange-600 hover:bg-gradient-to-br rounded-lg"
+                >
+                  Bewerkingen opslaan
+                </button>
+              </div>
+
+              <div v-else-if="buttonType === 2" class="float-right space-x-2 border-gray-200 mb-4">
+                <button disabled type="button" class="cursor-not-allowed p-2 text-md font-medium text-gray-400 bg-gray-200 rounded-lg">
+                  <svg role="status" class="inline mr-3 w-5 h-5 text-gray-400 animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
+                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
+                  </svg>
+                  Bewerkingen aan het opslaan...
+                </button>
+              </div>
+
+              <div v-else class="float-right space-x-2 border-gray-200 mb-4">
+                <button
+                    type="button"
+                    class="cursor-not-allowed p-2 text-md font-medium text-gray-400 bg-gray-200 rounded-lg"
+                    disabled
+                >
+                  Bewerkingen opslaan
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="px-4 py-3 h-auto" v-if="selectedSettings.id===2" v-bind="selectedSettings">
+          <div class="bg-white rounded-lg dark:bg-gray-700">
+            <div class="flex flex-col text-left">
+              <div class="flex items-start pt-2 mb-1 justify-between rounded-t dark:border-gray-600">
+                <h3 class="text-3xl 1 font-medium text-gray-900 dark:text-white">Account</h3>
+              </div>
+              <p class="text-md font-normal text-gray-600 pb-3">Deze informatie zal gebruikt worden om contact me u op te nemen.</p>
+            </div>
+
+            <hr>
+
+            <form class="h-fit pt-3">
+              <p class="block text-left mb-2 text-sm text-gray-600">Vul uw volledige naam in.</p>
+              <div class="grid md:grid-cols-3 md:gap-4">
                 <div class="relative z-0 mb-6 w-full group">
                   <label for="floating_first_name"
-                         class="block text-left mb-2 text-sm font-medium text-gray-900 dark:text-white">Voornaam
-                    name</label>
+                         class="block text-left mb-2 text-sm font-medium text-gray-900 dark:text-white">Voornaam</label>
                   <input @change="changeButtonToOne" v-model="this.user.first_name" type="text"
                          name="floating_first_name"
                          id="floating_first_name"
@@ -125,19 +193,64 @@
                          placeholder="Doe" required/>
                 </div>
               </div>
-              <div class="mb-4">
-                <label for="description"
-                       class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Bio</label>
-                <textarea @change="changeButtonToOne" v-model="this.user.bio" id="description" rows="4"
-                          class="block p-2.5 w-full h-24 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-orange-500 focus:border-orange-500"
-                          placeholder="Schrijf hier de beschrijving..."
-                ></textarea>
+              <p class="block text-left mb-2 text-sm text-gray-600">Vul uw contact gegevens in zodat wij contact met u kunnen opnemen.</p>
+              <div class="grid md:grid-cols-2 md:gap-4">
+                <div class="relative z-0 mb-6 w-full group">
+                  <label for="floating_first_name"
+                         class="block text-left mb-2 text-sm font-medium text-gray-900 dark:text-white">E-mailadres</label>
+                  <input v-model="this.user.email" type="email"
+                         name="floating_first_name"
+                         id="floating_first_name"
+                         class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 "
+                         placeholder="Johndoe@voorbeeld.nl" required/>
+                </div>
+                <div class="relative z-0 mb-6 w-full group">
+                  <label for="floating_first_name"
+                         class="block text-left mb-2 text-sm font-medium text-gray-900 dark:text-white">Telefoonnummer</label>
+                  <input  v-model="this.user.phone" type="text"
+                         name="floating_first_name"
+                         id="floating_first_name"
+                         class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 "
+                         placeholder="+31612345678" required/>
+                </div>
+              </div>
+
+              <p class="block text-left mb-2 text-sm text-gray-600">Vul uw locatiegegevens in.</p>
+              <div class="grid md:grid-cols-3 md:gap-4">
+                <div class="relative z-0 mb-6 w-full group">
+                  <label for="floating_first_name"
+                         class="block text-left mb-2 text-sm font-medium text-gray-900 dark:text-white">Adres</label>
+                  <input v-model="this.user.first_name" type="text"
+                         name="floating_first_name"
+                         id="floating_first_name"
+                         class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 "
+                         placeholder="Johndoe@voorbeeld.nl" required/>
+                </div>
+                <div class="relative z-0 mb-6 w-full group">
+                  <label for="floating_first_name"
+                         class="block text-left mb-2 text-sm font-medium text-gray-900 dark:text-white">Postcode</label>
+                  <input @change="changeButtonToOne" v-model="this.user.second_name" type="text"
+                         name="floating_first_name"
+                         id="floating_first_name"
+                         class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 "
+                         placeholder="+31612345678" required/>
+                </div>
+                <div class="relative z-0 mb-6 w-full group">
+                  <label for="floating_first_name"
+                         class="block text-left mb-2 text-sm font-medium text-gray-900 dark:text-white">Plaatsnaam</label>
+                  <input @change="changeButtonToOne" v-model="this.user.second_name" type="text"
+                         name="floating_first_name"
+                         id="floating_first_name"
+                         class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 "
+                         placeholder="+31612345678" required/>
+                </div>
               </div>
             </form>
 
             <hr>
 
             <div class="w-full mt-4">
+
               <div v-if="buttonType === 1" class="float-right space-x-2 border-gray-200 mb-4">
                 <button @click="updateClientProfile"
                         type="button"
@@ -146,6 +259,7 @@
                   Bewerkingen opslaan
                 </button>
               </div>
+
               <div v-else-if="buttonType === 2" class="float-right space-x-2 border-gray-200 mb-4">
                 <button disabled type="button" class="cursor-not-allowed p-2 text-md font-medium text-gray-400 bg-gray-200 rounded-lg">
                   <svg role="status" class="inline mr-3 w-5 h-5 text-gray-400 animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -163,6 +277,90 @@
                     disabled
                 >
                   Bewerkingen opslaan
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="px-4 py-3 h-auto" v-if="selectedSettings.id===3" v-bind="selectedSettings">
+          <div class="bg-white rounded-lg dark:bg-gray-700">
+            <div class="flex flex-col text-left">
+              <div class="flex items-start pt-2 mb-1 justify-between rounded-t dark:border-gray-600">
+                <h3 class="text-3xl 1 font-medium text-gray-900 dark:text-white">Wachtwoord</h3>
+              </div>
+              <p class="text-md font-normal text-gray-600 pb-3">Pas uw wachtwoord voor veiligheidsredenen</p>
+            </div>
+
+            <hr>
+
+            <form class="h-fit pt-3">
+              <div class="grid md:grid-cols-1 md:gap-4">
+                <div class="relative z-0 mb-6 w-full group">
+                  <label for="floating_first_name"
+                         class="block text-left mb-2 text-sm font-medium text-gray-900 dark:text-white">Huidig wachtwoord</label>
+                  <p class="block text-left mb-2 text-sm text-gray-600">Typ uw huidige wachtwoord zodat wij weten dat u het bent</p>
+                  <input v-model="this.user.password" type="password"
+                         name="floating_first_name"
+                         id="floating_first_name"
+                         class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 "
+                         placeholder="Johndoe@voorbeeld.nl" required/>
+                </div>
+              </div>
+
+              <p class="block text-left mb-2 text-sm text-gray-600">Typ uw nieuwe wachtwoord</p>
+              <div class="grid md:grid-cols-2 md:gap-4">
+                <div class="relative z-0 mb-6 w-full group">
+                  <label for="floating_first_name"
+                         class="block text-left mb-2 text-sm font-medium text-gray-900 dark:text-white">Nieuw wachtwoord</label>
+                  <input  v-model="this.user.password" type="password"
+                          name="floating_first_name"
+                          id="floating_first_name"
+                          class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 "
+                          placeholder="*******" required/>
+                </div>
+                <div class="relative z-0 mb-6 w-full group">
+                  <label for="floating_first_name"
+                         class="block text-left mb-2 text-sm font-medium text-gray-900 dark:text-white">Herhaal nieuw wachtwoord</label>
+                  <input  v-model="this.user.password" type="password"
+                          name="floating_first_name"
+                          id="floating_first_name"
+                          class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 "
+                          placeholder="*******" required/>
+                </div>
+              </div>
+            </form>
+
+            <hr>
+
+            <div class="w-full mt-4">
+
+              <div v-if="buttonType === 1" class="float-right space-x-2 border-gray-200 mb-4">
+                <button @click="updateClientProfile"
+                        type="button"
+                        class="p-2 text-md font-medium text-white bg-gradient-to-r from-orange-400 via-orange-500 to-orange-600 hover:bg-gradient-to-br rounded-lg"
+                >
+                  Wachtwoord opslaan
+                </button>
+              </div>
+
+              <div v-else-if="buttonType === 2" class="float-right space-x-2 border-gray-200 mb-4">
+                <button disabled type="button" class="cursor-not-allowed p-2 text-md font-medium text-gray-400 bg-gray-200 rounded-lg">
+                  <svg role="status" class="inline mr-3 w-5 h-5 text-gray-400 animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
+                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
+                  </svg>
+                  Wachtwoord aan het opslaan...
+                </button>
+              </div>
+
+              <div v-else class="float-right space-x-2 border-gray-200 mb-4">
+                <button
+                    type="button"
+                    class="cursor-not-allowed p-2 text-md font-medium text-gray-400 bg-gray-200 rounded-lg"
+                    disabled
+                >
+                  Wachtwoord opslan
                 </button>
               </div>
             </div>
@@ -224,14 +422,15 @@ export default {
             first_name: this.user.first_name,
             second_name: this.user.second_name,
             last_name: this.user.last_name,
+            bio: this.user.bio,
             phone: this.user.phone,
             userType: this.user.userType,
+            client: this.user.client
           })
           .then((res) => {
             this.changeButtonToTwo()
             // Get the current time in seconds
             let timer = 2;
-
             // Set an interval to run every 1000 milliseconds (1 second)
             let interval = setInterval(function () {
               // Decrement the timer by 1
