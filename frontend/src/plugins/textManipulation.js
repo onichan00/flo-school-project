c89f08a769fcfaa989188c63bb002932053f2eb1
Dennis Moes <dennis.moes2@hva.nl>
2023-01-06 12:03:01 +0100

    Added JSDoc

--- frontend/src/plugins/textManipulation.js
+++ frontend/src/plugins/textManipulation.js
@@ -1,5 +1,11 @@
 import dateFormat from "dateformat";
 
+/**
+ * Returns the full name of a specialist object by concatenating the first and last name and capitalizing the first letter of each
+ *
+ * @param {Object} specialist The specialist object
+ * @returns {string} The full name of the specialist
+ */
 export const specialistFullName = (specialist) => {
     return (String(specialist.first_name).charAt(0).toUpperCase() + String(specialist.first_name).slice(1))
         + ' '
@@ -7,10 +13,12 @@ export const specialistFullName = (specialist) => {
 }
 
 /**
- * Function that returns the full name of a user object by getting the first, second and last name and glueing them together
- * @param user User object
- * @param middleNameAbbreviation Boolean to decide if the second name output should be one letter with a dot or the full second name
- * @returns A string combining the first name, second name and last name with uppercase
+ * Returns the full name of a user object by concatenating the first, second (if provided), and last name and capitalizing the first letter of each.
+ * If middleNameAbbreviation is set to true, the second name will be abbreviated to just the first letter with a period.
+ *
+ * @param {Object} User The user object
+ * @param {boolean} middleNameAbbreviation Whether to abbreviate the second name to just the first letter with a period
+ * @returns {string} The full name of the user
  */
 export const userFullName = (user, middleNameAbbreviation = false) => {
     const firstName = firstLetterUpperCase(user.first_name);
@@ -20,12 +28,24 @@ export const userFullName = (user, middleNameAbbreviation = false) => {
     return `${firstName} ${secondName} ${lastName}`;
 }
 
+/**
+ * Capitalizes the first letter of a given string.
+ *
+ * @param {string} value THe string to be modified
+ * @returns {string} The modified string
+ */
 export const firstLetterUpperCase = (value) => {
     if (value) value = value.charAt(0).toUpperCase() + value.slice(1);
 
     return value;
 }
 
+/**
+ * Formats a given date.
+ *
+ * @param {Date} date The date to be formatted
+ * @returns {string} The formatted date
+ */
 export const formatDate = (date) => {
     return dateFormat(date, "dd-Mon-yyyy  hh:mm:ss")
 }
