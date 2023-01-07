38d9450211d71990cad263de412e85faadc2d549
Frank <frank.oud@hva.nl>
2023-01-07 20:25:34 +0100

    Admin | Client CRUD
    Clients can be updated, but WIP; needs more input fields

--- frontend/src/views/admin/client/ClientDetail.vue
+++ frontend/src/views/admin/client/ClientDetail.vue
@@ -9,21 +9,21 @@
                 <div class="relative z-0 mb-6 w-full group">
                   <label for="first_name"
                          class="block text-left mb-2 text-sm font-medium text-gray-900 dark:text-white">Voornaam</label>
-                  <input v-model="firstName" type="text" name="first_name" id="first_name"
+                  <input v-model="client.first_name" type="text" name="first_name" id="first_name"
                          class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 "
                          placeholder="John" required/>
                 </div>
                 <div class="relative z-0 mb-6 w-full group">
                   <label for="preposition"
                          class="block text-left mb-2 text-sm font-medium text-gray-900 dark:text-white">Tussenvoegsel (optioneel)</label>
-                  <input v-model="preposition" type="text" name="preposition" id="preposition"
+                  <input v-model="client.second_name" type="text" name="preposition" id="preposition"
                          class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 "
                          placeholder="van" required/>
                 </div>
                 <div class="relative z-0 mb-6 w-full group">
                   <label for="last_name"
                          class="block text-left mb-2 text-sm font-medium text-gray-900 dark:text-white">Achternaam</label>
-                  <input v-model="lastName" type="text" name="last_name" id="last_name"
+                  <input v-model="client.last_name" type="text" name="last_name" id="last_name"
                          class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 "
                          placeholder="Doe" required/>
                 </div>
@@ -31,13 +31,13 @@
               <div class="grid md:grid-cols-2 md:gap-6">
                 <div>
                   <label for="email" class="block text-left mb-2 text-sm font-medium text-gray-900 dark:text-white">Email adres</label>
-                  <input v-model="email" type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900
+                  <input v-model="client.email" type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900
                 sm:text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5
                 dark:focus:border-blue-500" placeholder="name@company.com" required>
                 </div>
                 <div>
                   <label for="phone" class="block text-left mb-2 text-sm font-medium text-gray-900 dark:text-white">Telefoonnummer</label>
-                  <input v-model="phone" type="phone" name="phone" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900
+                  <input v-model="client.phone" type="phone" name="phone" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900
                 sm:text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5
                 dark:focus:border-blue-500" placeholder="06 12 34 56 78" required>
                 </div>
@@ -46,13 +46,13 @@
             <div class="grid md:grid-cols-2 md:gap-6">
               <div>
                 <label for="password" class="block text-left mb-2 text-sm font-medium text-gray-900 dark:text-white">Wachtwoord</label>
-                <input v-model="password" type="password" name="password" id="password" placeholder="••••••••"
+                <input v-model="client.password" type="password" name="password" id="password" placeholder="••••••••"
                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 "
                        required>
               </div>
               <div>
                 <label for="repeat-password" class="block text-left mb-2 text-sm font-medium text-gray-900 dark:text-white">Herhaal Wachtwoord</label>
-                <input v-model="repeatPassword" type="password" name="password" id="repeat-password" placeholder="••••••••"
+                <input v-model="client.password" type="password" name="password" id="repeat-password" placeholder="••••••••"
                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 "
                        required>
               </div>
@@ -78,22 +78,13 @@
 </template>
 
 <script>
+
 export default {
   name: "ClientDetail",
   data() {
     return {
       id: this.$route.params.id,
-      firstName: null,
-      preposition: null,
-      lastName: null,
-      address: null,
-      zipCode: null,
-      city: null,
-      bio: null,
-      email: null,
-      password: null,
-      repeatPassword: null,
-      phone: null
+      client: {}
     }
   },
   created() {
@@ -109,29 +100,42 @@ export default {
           })
           .then(data => {
             const {first_name, second_name, last_name, address, zip_code, city, bio, email, phone} = data;
-            this.firstName = first_name;
-            this.preposition = second_name;
-            this.lastName = last_name;
-            this.address = address;
-            this.zipCode = zip_code;
-            this.city = city;
-            this.bio = bio;
-            this.email = email;
-            this.phone = phone;
+            this.client.id = this.id;
+            this.client.userType = 1;
+            this.client.first_name = first_name;
+            this.client.second_name = second_name;
+            this.client.last_name = last_name;
+            this.client.address = address;
+            this.client.zip_code = zip_code;
+            this.client.city = city;
+            this.client.bio = bio;
+            this.client.email = email;
+            this.client.phone = phone;
           });
     },
     updateClient() {
-
-    },
-    deleteClient() {
-      fetch(process.env.VUE_APP_API_URL + `/api/users/delete/${this.id}`, {
-        method: 'DELETE'
+      fetch(process.env.VUE_APP_API_URL + `/api/users/update`,{
+        method: 'PUT',
+        body: JSON.stringify(this.client),
+        headers: {
+          "Content-Type": "application/json"
+        }
       }).then(response => {
         if (response.ok) {
-          return response.json();
+          this.$router.push("/clients");
         }
-      });
-    }
+      })
+    },
+    //TODO decide what to do with 'deleted' clients
+    // deleteClient() {
+    //   fetch(process.env.VUE_APP_API_URL + `/api/users/delete/${this.id}`, {
+    //     method: 'DELETE'
+    //   }).then(response => {
+    //     if (response.ok) {
+    //       return response.json();
+    //     }
+    //   });
+    // }
   }
 }
 </script>
