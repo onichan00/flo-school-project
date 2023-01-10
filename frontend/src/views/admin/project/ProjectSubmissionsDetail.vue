45a08a18e1f0c056f6ffb3431114fc94168c1630
Ilias aachboun <ili.aachboun1@gmail.com>
2023-01-10 23:25:24 +0100

    paginatio

--- frontend/src/views/admin/project/ProjectSubmissionsDetail.vue
+++ frontend/src/views/admin/project/ProjectSubmissionsDetail.vue
@@ -244,8 +244,36 @@
         </tr>
 
         </tbody>
+
       </table>
+
     </div>
+
+    <nav aria-label="Page navigation example">
+      <ul class="inline-flex -space-x-px my-4">
+        <li>
+          <div class="px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</div>
+        </li>
+        <li>
+          <div class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</div>
+        </li>
+        <li>
+          <div class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</div>
+        </li>
+        <li>
+          <div aria-current="page" class="px-3 py-2 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</div>
+        </li>
+        <li>
+          <div class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">4</div>
+        </li>
+        <li>
+          <div class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">5</div>
+        </li>
+        <li>
+          <div class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</div>
+        </li>
+      </ul>
+    </nav>
 
     <br>
     <button v-on:click="deleteProject" type="button"
